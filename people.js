ef1d3e300470bdd132abc4047c21e0f523f98810
Dave Vause <dvause@gmail.com>
2014-03-17 14:26:52 -0400

    add app.js and data file. save dependencies. add .idea to gitignore

---
+++ people.js
@@ -0,0 +1,147 @@
+[
+	{
+		"firstName": "Sophie",
+		"lastName": "Craft",
+		"email": "sophiecraft@maroptic.com"
+	},
+	{
+		"firstName": "Gilbert",
+		"lastName": "Bonner",
+		"email": "gilbertbonner@maroptic.com"
+	},
+	{
+		"firstName": "Webster",
+		"lastName": "Kramer",
+		"email": "websterkramer@maroptic.com"
+	},
+	{
+		"firstName": "Martin",
+		"lastName": "Boyd",
+		"email": "martinboyd@maroptic.com"
+	},
+	{
+		"firstName": "Delores",
+		"lastName": "Nunez",
+		"email": "deloresnunez@maroptic.com"
+	},
+	{
+		"firstName": "Maribel",
+		"lastName": "Delacruz",
+		"email": "maribeldelacruz@maroptic.com"
+	},
+	{
+		"firstName": "Blanca",
+		"lastName": "Sparks",
+		"email": "blancasparks@maroptic.com"
+	},
+	{
+		"firstName": "Angelina",
+		"lastName": "Summers",
+		"email": "angelinasummers@maroptic.com"
+	},
+	{
+		"firstName": "Manning",
+		"lastName": "Porter",
+		"email": "manningporter@maroptic.com"
+	},
+	{
+		"firstName": "Perez",
+		"lastName": "Washington",
+		"email": "perezwashington@maroptic.com"
+	},
+	{
+		"firstName": "Ina",
+		"lastName": "Henderson",
+		"email": "inahenderson@maroptic.com"
+	},
+	{
+		"firstName": "Barnes",
+		"lastName": "Nolan",
+		"email": "barnesnolan@maroptic.com"
+	},
+	{
+		"firstName": "Clarice",
+		"lastName": "Holcomb",
+		"email": "clariceholcomb@maroptic.com"
+	},
+	{
+		"firstName": "Williamson",
+		"lastName": "Valentine",
+		"email": "williamsonvalentine@maroptic.com"
+	},
+	{
+		"firstName": "French",
+		"lastName": "Mcmillan",
+		"email": "frenchmcmillan@maroptic.com"
+	},
+	{
+		"firstName": "Trudy",
+		"lastName": "Sharpe",
+		"email": "trudysharpe@maroptic.com"
+	},
+	{
+		"firstName": "Daugherty",
+		"lastName": "Cherry",
+		"email": "daughertycherry@maroptic.com"
+	},
+	{
+		"firstName": "Mcintosh",
+		"lastName": "Christensen",
+		"email": "mcintoshchristensen@maroptic.com"
+	},
+	{
+		"firstName": "Deena",
+		"lastName": "Salinas",
+		"email": "deenasalinas@maroptic.com"
+	},
+	{
+		"firstName": "Annie",
+		"lastName": "Kim",
+		"email": "anniekim@maroptic.com"
+	},
+	{
+		"firstName": "Kathleen",
+		"lastName": "Chapman",
+		"email": "kathleenchapman@maroptic.com"
+	},
+	{
+		"firstName": "Luisa",
+		"lastName": "Donovan",
+		"email": "luisadonovan@maroptic.com"
+	},
+	{
+		"firstName": "Delgado",
+		"lastName": "Nieves",
+		"email": "delgadonieves@maroptic.com"
+	},
+	{
+		"firstName": "Livingston",
+		"lastName": "Sears",
+		"email": "livingstonsears@maroptic.com"
+	},
+	{
+		"firstName": "Tonia",
+		"lastName": "Stokes",
+		"email": "toniastokes@maroptic.com"
+	},
+	{
+		"firstName": "Lauri",
+		"lastName": "Pierce",
+		"email": "lauripierce@maroptic.com"
+	},
+	{
+		"firstName": "Dianna",
+		"lastName": "Gibson",
+		"email": "diannagibson@maroptic.com"
+	},
+	{
+		"firstName": "Karin",
+		"lastName": "Booker",
+		"email": "karinbooker@maroptic.com"
+	},
+	{
+		"firstName": "Theresa",
+		"lastName": "Wise",
+		"email": "theresawise@maroptic.com"
+	}
+]
