08444b09a93a32368b384f0c51f96b3481be3bcb
Dave Vause <dvause@gmail.com>
2014-04-01 11:34:03 -0400

    people controller test with mock peopleService

--- app/people/people.spec.js
+++ app/people/people.spec.js
@@ -1,16 +1,34 @@
 describe('Unit Test: people module', function() {
-	var peopleCtrl;
-	var $scope;
+	var controller;
+	var scope;
+	var peopleService;
+	var q;
+	var deferred;
 
 	beforeEach(module("people"));
-	beforeEach(inject(function($controller, $rootScope) {
-		$scope = $rootScope.$new();
-		peopleCtrl = $controller("PeopleController", {$scope: $scope})
+
+	beforeEach(inject(function($controller, $rootScope, $q) {
+		peopleService = {
+			getPeople: function() {
+				deferred = q.defer();
+				return deferred.promise;
+			}
+		};
+		scope = $rootScope.$new();
+		q = $q;
+		controller = $controller("PeopleController", {$scope: scope, PeopleService: peopleService})
+		spyOn(peopleService, 'getPeople').andCallThrough();
 	}));
 
-	describe("PeopleController", function() {
+	describe("PeopleController" ,function() {
 		it("should fetch a list of people from a backend service", function() {
-
+			controller.init();
+			deferred.resolve();
+			scope.$digest();
+			expect(peopleService.getPeople).toHaveBeenCalled();
 		})
 	})
+
+
+
 });
