ff8df06403cdda94e526f41f8fc78df4754d615d
Dave Vause <dvause@gmail.com>
2014-04-01 14:29:47 -0400

    got jasmine spies working

--- app/people/people.spec.js
+++ app/people/people.spec.js
@@ -1,34 +1,50 @@
+var peopleData = [
+	{firstName: 'Annie', lastName: 'Kim', email: 'anniekim@maroptic.com' },
+	{firstName: 'Jane', lastName: 'Doe', email: 'jdoe@maroptic.com' },
+];
+var personData = {firstName: 'Annie', lastName: 'Kim', email: 'anniekim@maroptic.com' };
+
 describe('Unit Test: people module', function() {
-	var controller;
-	var scope;
+	var $controller;
+	var peopleCtrl;
+	var $scope;
 	var peopleService;
-	var q;
-	var deferred;
 
 	beforeEach(module("people"));
 
-	beforeEach(inject(function($controller, $rootScope, $q) {
-		peopleService = {
-			getPeople: function() {
-				deferred = q.defer();
-				return deferred.promise;
-			}
-		};
-		scope = $rootScope.$new();
-		q = $q;
-		controller = $controller("PeopleController", {$scope: scope, PeopleService: peopleService})
-		spyOn(peopleService, 'getPeople').andCallThrough();
+	beforeEach(inject(function(_$controller_, $rootScope, $q, _PeopleService_) {
+		$controller = _$controller_;
+		$scope = $rootScope.$new();
+		peopleService = _PeopleService_;
+
+		spyOn(peopleService, 'getPeople').andCallFake(function() {
+			return $q.when(peopleData);
+		});
+
+		spyOn(peopleService, 'getPerson').andCallFake(function() {
+			return $q.when(personData);
+		});
+
 	}));
 
+	function createController() {
+		peopleCtrl = $controller('PeopleController', {$scope: $scope});
+	}
+
 	describe("PeopleController" ,function() {
-		it("should fetch a list of people from a backend service", function() {
-			controller.init();
-			deferred.resolve();
-			scope.$digest();
+		it("should fetch a list of people from a backend service and populate the peopleCtrl.people property", function() {
+			createController();
+			peopleCtrl.init(); //get the list of people by calling the peopleService
+			$scope.$digest(); //run the digest cycle
 			expect(peopleService.getPeople).toHaveBeenCalled();
+			expect(peopleCtrl.people.length).not.toBe(0);
+		});
+
+		it("should populate the selectedPerson when selectPerson() is called", function() {
+			createController();
+			peopleCtrl.selectPerson(personData);
+			$scope.$digest();
+			expect(peopleCtrl.selectedPerson).not.toBe(undefined);
 		})
 	})
-
-
-
 });
